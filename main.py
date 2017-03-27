2bbe0d7643f9a5e36f85842b0ea371f0ef1475f5
Mike Eubanks <mooneyoak@gmail.com>
2017-03-27 18:40:47 -0500

    working?

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import webapp2
 
 
-# html boilerplate for the top of every page
 page_header = """
 <!DOCTYPE html>
 <html>
@@ -12,16 +11,13 @@ page_header = """
     <h1>FlickList</h1>
 """
 
-# html boilerplate for the bottom of every page
 page_footer = """
 </body>
 </html>
 """
 
 class Index(webapp2.RequestHandler):
-    """ Handles requests coming in to '/' (the root of our site)
-        e.g. www.flicklist.com/
-    """
+
 
     def get(self):
 
@@ -39,8 +35,6 @@ class Index(webapp2.RequestHandler):
         </form>
         """
 
-    def get(self):
-
         edit_header = "<h3>Edit My Watchlist</h3>"
 
     # a form for adding new movies
@@ -56,12 +50,6 @@ class Index(webapp2.RequestHandler):
         """
 
 
-
-        # TODO 4 (Extra Credit)
-        # modify your form to use a dropdown (<select>) instead a
-        # text box (<input type="text"/>)
-
-
         content = page_header + edit_header + add_form + page_footer
         self.response.write(content)
 
@@ -72,36 +60,16 @@ class AddMovie(webapp2.RequestHandler):
     """
 
     def post(self):
-        # look inside the request to figure out what the user typed
-        new_movie = self.request.get("new-movie")
-
-        # build response content
-        new_movie_element = "<strong>" + new_movie + "</strong>"
-        sentence = new_movie_element + " has been added to your Watchlist!"
-
-        content = page_header + "<p>" + sentence + "</p>" + page_footer
         self.response.write(content)
 
 
 class RemoveMovie(webapp2.RequestHandler):
-    """ Handles requests coming in to '/remove'
-        e.g. www.flicklist.com/remove
-    """
 
     def post(self):
-        # look inside the request to figure out what the user typed
-        watched_movie = self.request.get("watched-movie")
 
-        # build response content
-        watched_movie_element = "<strike>" + watched_movie + "</strike>"
-        sentence = watched_movie_element + " has been removed from your Watchlist!"
-
-        content = page_header + "<p>" + sentence + "</p>" + page_footer
         self.response.write(content)
 
 
-# TODO 3
-# Include a route for your cross-off handler, by adding another tuple to the list below.
 app = webapp2.WSGIApplication([
     ('/', Index),
     ('/add', AddMovie),
